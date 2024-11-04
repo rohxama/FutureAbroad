f8f55a9133f669fc5b76209b611b9b1e8a482ce4
Rohama Salfi <rohxama@gmail.com>
2024-11-03 19:23:17 -0800

    error solve krrahi thi lakin nhi howa

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@ import { Header } from "@/components/Header";
 import { HeroSection } from "@/components/Herosection";
 import { AboutSection } from "@/components/Aboutsection";
 
+
 export default function Home() {
   return (
     <div className="min-h-screen">
