a8d566240ac34534cefe562da1b358b3be6b07a3
Rohama Salfi <rohxama@gmail.com>
2024-11-03 18:26:48 -0800

    add herosection components in page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import { Header } from "@/components/Header";
+import { HeroSection } from "@/components/Herosection";
 
 export default function Home() {
   return (
     <div className="min-h-screen">
       <Header />
+      <HeroSection />
     </div>
   );
 }
