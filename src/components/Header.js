7495b1ce94b1191e4d9ba32b777400cc6c9adfca
Rohama Salfi <rohxama@gmail.com>
2024-11-03 17:40:49 -0800

    font added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,11 @@
 
 import Link from 'next/link';
-// import styles from '../styles/Header.css'; 
 
 export const Header = () => {
 	return (
 		<header className='w-full bg-green-500'>
 				<nav className='flex flex-row gap-4 items-center justify-between max-w-6xl mx-auto p-3'>
-						<Link href="/">FutureAboard</Link>
+						<Link href="/" className='font-semibold text-4xl'>FutureAboard</Link>
 							<ul className='flex flex-row gap-4'>
 								<li><Link href="#">Home</Link></li>
 								<li><Link href="#">Services</Link></li>
