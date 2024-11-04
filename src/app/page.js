2dc72e47eadb3503890d682203bcdb4d11322dc1
Rohama Salfi <rohxama@gmail.com>
2024-11-03 19:18:05 -0800

    add about section in page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import { Header } from "@/components/Header";
 import { HeroSection } from "@/components/Herosection";
+import { AboutSection } from "@/components/Aboutsection";
 
 export default function Home() {
   return (
     <div className="min-h-screen">
       <Header />
       <HeroSection />
+      <AboutSection />
     </div>
   );
 }
